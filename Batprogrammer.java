840e6948c23f37f486384d4c90ba59c5d98cbd2e
Madhubalanramu <115461140+Madhubalanramu@users.noreply.github.com>
2022-10-12 15:47:44 +0530

    Add files via upload

---
+++ Batprogrammer.java
@@ -0,0 +1,11 @@
+public class Batprogrammer
+{
+    public static Integer stepsRequired(Integer prisoners, Integer steps){
+        int k=prisoners*steps;
+    return (k);
+}
+ 
+	public static void main(String[] args) {
+		System.out.println("Number of Steps: " + stepsRequired(3, 10).toString());
+	}
+}
